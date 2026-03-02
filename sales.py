436f26ea81e69f3decfacb340ad63ef049b921dc
Allan-dev-glitch <your_email@example.com>
2026-03-01 23:02:55 -0800

    changed sales module

--- sales.py
+++ sales.py
@@ -1,5 +1,4 @@
-from shop_counter.inventory import update_stock
-from shop_counter.data import products
+
 import json
 
 
@@ -13,13 +12,3 @@ class Sale:
         sale = {"item": self.item, "quantity": self.qty, "total": self.total}
         with open("sales_history.json", "a") as f:
             f.write(json.dumps(sale) + "\n")
-
-def buy_item(item, quantity):
-    if update_stock(item, quantity):
-        total = products[item]["price"] * quantity
-        print(f"✅ You bought {quantity} {item}(s) for {total} KES.")
-       
-        sale = Sale(item, quantity, total)
-        sale.save()
-    else:
-        print("❌ Not enough stock or item not found.")
